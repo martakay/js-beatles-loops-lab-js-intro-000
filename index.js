b622ed482bf3475ed6eb8809632e33b5e7c32cc5
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-16 19:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var stuff = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    stuff.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return stuff;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts.push(facts[i] + "!!!");
+  }
+  return facts;
+}
